6a980d2a8271951aec18b5bb23679950502d5fe1
IHannes <104006031+IHannes@users.noreply.github.com>
2023-04-10 10:12:28 +0200

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NOTE=$(zenity --title"Title" --text "Insert title of yor Note" --entry --width 450)
+NOTE=$(zenity --title="Title" --text "Insert title of yor Note" --entry --width 450)
 PWD=$(zenity --title="Password" --text "Insert your Password here" --entry --width 450)
 keytool -genseckey -alias "$NOTE" -keypass "$PWD" -keyalg AES -keysize 128 -keystore notes.pfx -storepass "$PWD" -storetype PKCS12 -v
 
